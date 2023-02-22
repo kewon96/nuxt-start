2993f9127448c949b4c3d03437b0c336b95c0b96
Kewon <yang0184@naver.com>
2023-02-23 01:23:19 +0900

    Adding Pages

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<!-- /index -->
+
+<template>
+  <h2>Home</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid autem blanditiis, deserunt dolorem doloribus eius, facere iste, libero molestiae nobis odio omnis provident quam quod ut voluptate! Quasi, tempore!</p>
+</template>
+
+<script setup lang="ts">
+
+/******** Type & Interface **********/
+
+
+/******** Instance **********/
+
+
+/******** Reactive Instance **********/
+
+
+/******** Hooks **********/
+
+
+/******** Functions **********/
+
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+</style>
---
+++ pages/products/hello.vue
@@ -0,0 +1,36 @@
+<!-- /products/hello -->
+
+<template>
+  <h2>Hello</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid autem blanditiis, deserunt dolorem doloribus eius, facere iste, libero molestiae nobis odio omnis provident quam quod ut voluptate! Quasi, tempore!</p>
+</template>
+
+<script setup lang="ts">
+
+/******** Type & Interface **********/
+
+
+/******** Instance **********/
+
+
+/******** Reactive Instance **********/
+
+
+/******** Hooks **********/
+
+
+/******** Functions **********/
+
+
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
